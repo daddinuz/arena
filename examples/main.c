b45d9fd4b6c34254892e5c3043aee5ea4cd0ef4d
daddinuz <daddinuz@gmail.com>
2020-05-14 23:37:43 +0200

    update examples

--- examples/main.c
+++ examples/main.c
@@ -1,93 +1,56 @@
 /*
-Author: daddinuz
-email:  daddinuz@gmail.com
-
-Copyright (c) 2018 Davide Di Carlo
-
-Permission is hereby granted, free of charge, to any person
-obtaining a copy of this software and associated documentation
-files (the "Software"), to deal in the Software without
-restriction, including without limitation the rights to use,
-copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2020 Davide Di Carlo
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
  */
 
+#include <stdalign.h>
+#include <assert.h>
 #include <stdio.h>
 #include <arena.h>
-#include <string.h>
-#include <assert.h>
-#include <stdalign.h>
 
-static void printArena(const struct Arena *const arena) {
-    printf("Arena(size=%zu, slop=%zu, capacity=%zu, bestAvailable=%zu, worstAvailable=%zu, chunks=%zu)\n",
-           Arena_getSize(arena), Arena_getSlop(arena), Arena_getCapacity(arena), Arena_getBestAvailable(arena),
-           Arena_getWorstAvailable(arena), Arena_getChunks(arena));
-}
-
-static char *String_new(struct Arena *const arena, const char *const literal) {
-    assert(arena);
-    assert(literal);
-    const size_t size = strlen(literal);
-    char *self = Arena_requestWithAlignment(arena, alignof(char), size + 1);
-    strncpy(self, literal, size);
-    self[size] = 0;
-    return self;
-}
-
-struct Person {
-    char *firstName;
-    char *lastName;
-};
-
-static struct Person *Person_new(struct Arena *const arena, const char *const firstName, const char *const lastName) {
-    assert(arena);
-    assert(firstName);
-    assert(lastName);
-    struct Person *self = Arena_request(arena, sizeof(*self));
-    self->firstName = String_new(arena, firstName);
-    self->lastName = String_new(arena, lastName);
-    return self;
-}
-
-static void Person_print(const struct Person *const self) {
-    assert(self);
-    printf("Person(firstName=\"%s\", lastName=\"%s\")\n", self->firstName, self->lastName);
-}
+static void printArena(const struct Arena *self);
 
 int main() {
-    struct Arena *arena = Arena_new();
+    struct Arena *const arena = Arena_default();
+    const long double number = 42.0f;
+    const char string[] = "Hello World!\n";
+
+    printArena(arena);
+    const long double *const a = Arena_clone(arena, &number, alignof(number), sizeof(number));
+    const char *const b = Arena_clone(arena, &string, alignof(string), sizeof(string));
+    const int *const c = Arena_allocate(arena, alignof(*c), sizeof(*c));
     printArena(arena);
 
-    struct Person *giuseppeGaribaldi = Person_new(arena, "Giuseppe", "Garibaldi");
-    printArena(arena);
+    printf("%sThe number of the day is: %LF\nUninitialized value: %d\n", b, *a, *c);
 
-    struct Person *vittorioEmanuele = Person_new(arena, "Vittorio", "Emanuele");
-    printArena(arena);
-
-    Person_print(giuseppeGaribaldi);
-    Person_print(vittorioEmanuele);
-
-    Arena_clear(arena);
-    printArena(arena);
-
-    struct Person *camilloBenso = Person_new(arena, "Camillo", "Benso");
-    printArena(arena);
-
-    Person_print(camilloBenso);
-
-    Arena_delete(arena);
+    Arena_drop(arena);
     return 0;
+}
+
+static void printArena(const struct Arena *const self) {
+    assert(NULL != self);
+    printf("Arena(available=%zu, capacity=%zu, size=%zu)\n",
+           Arena_available(self), Arena_capacity(self), Arena_size(self));
 }
