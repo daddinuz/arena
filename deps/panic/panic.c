76292532f692d645beab8541d23824d4bd3eb59d
daddinuz <daddinuz@gmail.com>
2020-05-14 23:35:58 +0200

    update deps

--- deps/panic/panic.c
+++ deps/panic/panic.c
@@ -1,8 +1,7 @@
 /*
- * Author: daddinuz
- * email:  daddinuz@gmail.com
+ * The MIT License (MIT)
  *
- * Copyright (c) 2018 Davide Di Carlo
+ * Copyright (c) 2019 Davide Di Carlo
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -33,33 +32,135 @@
 #include <assert.h>
 #include "panic.h"
 
-static Panic_Callback globalCallback = NULL;
+#define NEWLINE "\r\n"
 
-Panic_Callback Panic_registerCallback(const Panic_Callback callback) {
-    const Panic_Callback backup = callback;
-    globalCallback = callback;
+static PanicHandler globalPanicHandler = NULL;
+
+static void terminate(const char *trace, const char *format, va_list args)
+__attribute__((__noreturn__, __nonnull__(1, 2), __format__(__printf__, 2, 0)));
+
+static void backtrace(FILE *stream)
+__attribute__((__nonnull__));
+
+PanicHandler panic_register(const PanicHandler handler) {
+    const PanicHandler backup = globalPanicHandler;
+    globalPanicHandler = handler;
     return backup;
 }
 
-void __Panic_terminate(const char *file, int line, const char *format, ...) {
-    assert(file);
-    assert(format);
+void __panic(const char *const trace, const char *const format, ...) {
+    assert(NULL != trace);
+    assert(NULL != format);
     va_list args;
     va_start(args, format);
-    __Panic_vterminate(file, line, format, args);
+    terminate(trace, format, args);
 }
 
-void __Panic_vterminate(const char *file, int line, const char *format, va_list args) {
-    assert(file);
-    assert(format);
-    fprintf(stderr, "\nAt: %s:%d\n", file, line);
-    if (errno != 0) {
-        fprintf(stderr, "errno: (%d) %s\n", errno, strerror(errno));
+void __vpanic(const char *const trace, const char *const format, va_list args) {
+    assert(NULL != trace);
+    assert(NULL != format);
+    terminate(trace, format, args);
+}
+
+void __panic_when(const bool condition, const char *const trace, const char *const format, ...) {
+    assert(NULL != trace);
+    assert(NULL != format);
+    if (condition) {
+        va_list args;
+        va_start(args, format);
+        terminate(trace, format, args);
     }
+}
+
+void __panic_unless(const bool condition, const char *const trace, const char *const format, ...) {
+    assert(NULL != trace);
+    assert(NULL != format);
+    if (!condition) {
+        va_list args;
+        va_start(args, format);
+        terminate(trace, format, args);
+    }
+}
+
+void terminate(const char *const trace, const char *const format, va_list args) {
+    assert(NULL != trace);
+    assert(NULL != format);
+    fputs(NEWLINE, stderr);
+    backtrace(stderr);
+    fprintf(stderr, "   At: '%s'" NEWLINE, trace);
+
+    if (0 != errno) {
+        fprintf(stderr, "Error: (%d) %s" NEWLINE, errno, strerror(errno));
+    }
+
+    fputs("Cause: ", stderr);
     vfprintf(stderr, format, args);
+    fputs(NEWLINE, stderr);
     va_end(args);
-    if (globalCallback) {
-        globalCallback();
+
+    if (NULL != globalPanicHandler) {
+        globalPanicHandler();
     }
+
     abort();
 }
+
+#if PANIC_UNWIND_SUPPORT
+
+#define N_SIZE 8
+#define M_SIZE 32
+
+#define UNW_LOCAL_ONLY
+
+#include <libunwind.h>
+
+void backtrace(FILE *const stream) {
+    assert(NULL != stream);
+    char buffer[N_SIZE][M_SIZE + 1] = {{0}};
+    unw_context_t context = {0};
+    unw_cursor_t cursor = {0};
+    const int previousError = errno;
+    size_t size = 0;
+
+    unw_getcontext(&context);
+    unw_init_local(&cursor, &context);
+
+    // skip: terminate and panics function calls
+    for (size_t i = 0; i < 2; ++i) {
+        if (unw_step(&cursor) <= 0) {
+            errno = previousError;  // restore errno
+            return;                 // something wrong, exit
+        }
+    }
+
+    while (size < N_SIZE &&
+           unw_step(&cursor) > 0 &&
+           unw_get_proc_name(&cursor, buffer[size], M_SIZE, NULL) == 0 &&
+           strcmp("main", buffer[size++]) != 0);
+
+    if (0 < size) {
+        fputs("Traceback (most recent call last):" NEWLINE, stream);
+
+        if (0 != strcmp("main", buffer[size - 1])) {
+            fputs("  [ ]: (...)" NEWLINE, stream);
+        }
+
+        for (size_t i = 1; i < size; ++i) {
+            fprintf(stream, "  [%zu]: (%s)" NEWLINE, i - 1, buffer[size - i]);
+        }
+
+        fprintf(stream, "  ->-: (%s) current function" NEWLINE, buffer[0]);
+        fputs(NEWLINE, stream);
+    }
+
+    errno = previousError;  // restore errno
+}
+
+#else
+
+void backtrace(FILE *const stream) {
+    assert(NULL != stream);
+    (void) stream;
+}
+
+#endif
